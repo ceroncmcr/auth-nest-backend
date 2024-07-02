23ddb5b29ab2adac70105bda318e46c8069ebe98
Cesar Ceron <ceron.cmcr@gmail.com>
2024-07-02 16:57:55 -0600

    Curso Angular - Seccion 25

---
+++ README.md
@@ -0,0 +1,4 @@
+# Backend en Nest
+
+1. Ejecutar el commando ```docker compose up -d```
+2. Copiar el archivo ```.env.template``` y renombrarlo a .env
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { User, UserSchema } from './entities/user.entity';
+
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService],
+  imports: [
+    ConfigModule.forRoot(),
+
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema
+      }
+    ]),
+
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SEED,
+      signOptions: { expiresIn: '6h' },//{ expiresIn: '60s' },
+    }),
+  ],
+})
+export class AuthModule {}
