19c650599556e6e86dc5e9ff6ede82c772d6ce73
Cesar Ceron <ceron.cmcr@gmail.com>
2024-07-04 17:56:07 -0600

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     })
   );
-
 
   await app.listen(3000);
 }
